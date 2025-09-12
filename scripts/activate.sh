0fc35b11199d2416eb1dc45066243c933bb2b5c5
haok1402 <haok1402@gmail.com>
2025-09-11 21:23:51 -0400

    to avoid rate limit, let's use the built-in hf download capabilities

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-# Specify the dataset mounting paths.
-DATASET_LOCAL=/scratch/$USER/ProLong/datasets
-DATASET_MOUNT=$PWD/datasets
-
-# Specify the checkpoints mounting paths.
-CHECKPOINTS_LOCAL=/scratch/$USER/ProLong/checkpoints
-CHECKPOINTS_MOUNT=$PWD/checkpoints
-
-# Mount from local SSD to current workspace.
-mkdir -p $DATASET_LOCAL
-if [ ! -e $DATASET_MOUNT ]; then
-    ln -s $DATASET_LOCAL $DATASET_MOUNT
-fi
-mkdir -p $CHECKPOINTS_LOCAL
-if [ ! -e $CHECKPOINTS_MOUNT ]; then
-    ln -s $CHECKPOINTS_LOCAL $CHECKPOINTS_MOUNT
-fi
-
-# Download the datasets if not already present.
-if [ ! -d $DATASET_MOUNT/long-context-65536 ]; then
-    git clone https://huggingface.co/datasets/princeton-nlp/prolong-data-64K datasets/long-context-65536
-fi
-if [ ! -d $DATASET_MOUNT/long-context-524288 ]; then
-    git clone https://huggingface.co/datasets/princeton-nlp/prolong-data-512K datasets/long-context-524288
-fi
-if [ ! -d $DATASET_MOUNT/prolong-ultrachat-64K ]; then
-    git clone https://huggingface.co/datasets/princeton-nlp/prolong-ultrachat-64K datasets/prolong-ultrachat-64K
-fi
-
 # Activate the conda environment.
 source /home/$USER/miniconda3/etc/profile.d/conda.sh
 conda activate ProLong
@@ -37,3 +8,34 @@ conda activate ProLong
 export CUDA_HOME=/usr/local/cuda-11.8
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+# Use cluster-wide cache directory for HuggingFace models.
+export HF_HUB_CACHE=/data/hf_cache
+
+# Setup directories for datasets and checkpoints.
+DATASET_LOCAL=/scratch/$USER/ProLong/datasets
+DATASET_MOUNT=$PWD/datasets
+
+mkdir -p $DATASET_LOCAL
+if [ ! -e $DATASET_MOUNT ]; then
+    ln -s $DATASET_LOCAL $DATASET_MOUNT
+fi
+
+CHECKPOINTS_LOCAL=/scratch/$USER/ProLong/checkpoints
+CHECKPOINTS_MOUNT=$PWD/checkpoints
+
+mkdir -p $CHECKPOINTS_LOCAL
+if [ ! -e $CHECKPOINTS_MOUNT ]; then
+    ln -s $CHECKPOINTS_LOCAL $CHECKPOINTS_MOUNT
+fi
+
+# Download the datasets if not already present.
+if [ ! -d $DATASET_MOUNT/long-context-65536 ]; then
+    hf download --repo-type dataset princeton-nlp/prolong-data-64K --local-dir datasets/long-context-65536
+fi
+if [ ! -d $DATASET_MOUNT/long-context-524288 ]; then
+    hf download --repo-type dataset princeton-nlp/prolong-data-512K --local-dir datasets/long-context-524288
+fi
+if [ ! -d $DATASET_MOUNT/prolong-ultrachat-64K ]; then
+    hf download --repo-type dataset princeton-nlp/prolong-ultrachat-64K --local-dir datasets/prolong-ultrachat-64K
+fi
