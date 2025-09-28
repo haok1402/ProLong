4269ba38dbebbe1424787165f55575a3e61adb97
haok1402 <haok1402@gmail.com>
2025-09-28 10:37:38 -0400

    try to start with a smaller learning rate and see how things work out

--- scripts/train_64K.sh
+++ scripts/train_64K.sh
@@ -10,7 +10,7 @@ source scripts/activate.sh
 
 # Use Qwen3-0.6B. A small model, so FSDP is not necessary.
 # We train 4B tokens in one day on 8XH100 GPUs.
-MODEL=Qwen/Qwen3-0.6B; FSDP=0; NSA=1; LR=1e-4
+MODEL=Qwen/Qwen3-0.6B; FSDP=0; NSA=1
 DATASET=datasets/Qwen3/long-context-65536; STEPS=1000
 
 # Following ablations for position extrapolation in B.1 of the paper,
